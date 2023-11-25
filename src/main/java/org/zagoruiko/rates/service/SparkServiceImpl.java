135c41594d10a093b74af2a444b7e0bce5690cbb
andy <a@zagoruiko.org.ua>
2023-11-25 22:57:05 +0100

    fix image tag

--- src/main/java/org/zagoruiko/rates/service/SparkServiceImpl.java
+++ src/main/java/org/zagoruiko/rates/service/SparkServiceImpl.java
@@ -24,7 +24,7 @@ public class SparkServiceImpl implements SparkService {
         spark.sql("CREATE EXTERNAL TABLE currencylayer " +
                 "(date DATE, asst STRING, quote STRING, rate FLOAT) " +
                 "ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' " +
-                "PARTITIONED BY (asset STRING) " +
+                "PARTITIONED BY (asset STRING, quote STRING) " +
                 "LOCATION 's3a://currency/currencylayer/' " );
 
         repairCurrenciesTables();
